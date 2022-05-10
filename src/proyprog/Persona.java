5f51bd1472527768291377a08a73c3b3a4a1e0b3
4lvaro321 <99471055+4lvaro321@users.noreply.github.com>
2022-05-10 09:46:10 +0200

    Update Persona.java

--- src/proyprog/Persona.java
+++ src/proyprog/Persona.java
@@ -10,8 +10,16 @@ public class Persona {
     /**
      * Default constructor
      */
-    public Persona() {
+    public Persona(String Nombre, String Apellidos, String DNI, long Telefono, String Email, String Contraseña, String Usuario) {
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.DNI = DNI;
+        this.Telefono = Telefono;
+        this.Email = Email;
+        this.Contraseña = Contraseña;
+        this.Usuario = Usuario;
     }
+
 
     
     private String Nombre;
