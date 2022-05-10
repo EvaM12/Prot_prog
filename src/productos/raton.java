274a8e9ed5ed441eb73b29903a5a80622da41078
funerari <99471650+funerari@users.noreply.github.com>
2022-05-10 09:40:29 +0200

    Update raton.java

--- src/productos/raton.java
+++ src/productos/raton.java
@@ -5,11 +5,30 @@ import java.util.*;
 
 public class raton extends Producto{
 
-    public raton() {
-    }
-
     private String Sensor;
-
     private int DPi;
 
+    public String getSensor() {
+        return Sensor;
+    }
+
+    public void setSensor(String Sensor) {
+        this.Sensor = Sensor;
+    }
+
+    public int getDPi() {
+        return DPi;
+    }
+
+    public void setDPi(int DPi) {
+        this.DPi = DPi;
+    }
+
+    public raton(String Sensor, int DPi, String nombre, float precio, String marca, int stock, float PVP) {
+        super(nombre, precio, marca, stock, PVP);
+        this.Sensor = Sensor;
+        this.DPi = DPi;
+    }
+
 }
+
