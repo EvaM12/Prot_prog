b26ebf907f738ec72c8c6f6ee2628b6d549dcc5b
funerari <99471650+funerari@users.noreply.github.com>
2022-05-10 09:39:45 +0200

    Update refigeracion.java

--- src/productos/refigeracion.java
+++ src/productos/refigeracion.java
@@ -5,9 +5,20 @@ import java.util.*;
 
 public class refigeracion extends Producto{
 
-    public refigeracion() {
-    }
-
     private String Tipo;
 
+    public refigeracion(String Tipo, String nombre, float precio, String marca, int stock, float PVP) {
+        super(nombre, precio, marca, stock, PVP);
+        this.Tipo = Tipo;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    
 }
