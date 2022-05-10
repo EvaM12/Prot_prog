fb4eb46c313b3f8a5745558bee5e36bacdc49526
funerari <99471650+funerari@users.noreply.github.com>
2022-05-10 09:41:20 +0200

    Update procesador.java

--- src/productos/procesador.java
+++ src/productos/procesador.java
@@ -4,16 +4,57 @@ import proyprog.Producto;
 import java.util.*;
 
 public class procesador extends Producto{
+    private String Socket;
+    private float Frecuencia;
+   private int Nucleos;
+    private int Hilos;
+    
+    
 
-    public procesador() {
+    public String getSocket() {
+        return Socket;
     }
 
-    private String Socket;
+    public float getFrecuencia() {
+        return Frecuencia;
+    }
 
-    private float Frecuencia;
+    public int getNucleos() {
+        return Nucleos;
+    }
 
-    private int Nucleos;
+    public int getHilos() {
+        return Hilos;
+    }
 
-    private int Hilos;
+    public void setSocket(String Socket) {
+        this.Socket = Socket;
+    }
+
+    public void setFrecuencia(float Frecuencia) {
+        this.Frecuencia = Frecuencia;
+    }
+
+    public void setNucleos(int Nucleos) {
+        this.Nucleos = Nucleos;
+    }
+
+    public void setHilos(int Hilos) {
+        this.Hilos = Hilos;
+    }
+
+
+    public procesador(String Socket, float Frecuencia, int Nucleos, int Hilos, String nombre, float precio, String marca, int stock, float PVP) {
+        super(nombre, precio, marca, stock, PVP);
+        this.Socket = Socket;
+        this.Frecuencia = Frecuencia;
+        this.Nucleos = Nucleos;
+        this.Hilos = Hilos;
+    }
+
+   
+    
+    
 
 }
+
