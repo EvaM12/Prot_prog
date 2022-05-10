f9fc667d89437000261463ea46e77fa00eb42793
EvaM12 <61553044+EvaM12@users.noreply.github.com>
2022-05-10 09:40:50 +0200

    Update PlacaBase.java

--- src/productos/PlacaBase.java
+++ src/productos/PlacaBase.java
@@ -5,11 +5,28 @@ import java.util.*;
 
 public class PlacaBase extends Producto{
 
-    public PlacaBase() {
+    public PlacaBase(String Socket, String Tipo, String nombre, float precio, String marca, int stock, float PVP) {
+        super(nombre, precio, marca, stock, PVP);
+        this.Socket = Socket;
+        this.Tipo = Tipo;
+    }
+
+    public String getSocket() {
+        return Socket;
+    }
+
+    public void setSocket(String Socket) {
+        this.Socket = Socket;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
     }
 
     private String Socket;
-
     private String Tipo;
-
 }
