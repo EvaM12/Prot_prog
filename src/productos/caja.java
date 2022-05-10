0e0331cc4c6ddfd7b1324ae84a37f1e5365cfead
EvaM12 <61553044+EvaM12@users.noreply.github.com>
2022-05-10 09:41:45 +0200

    Update caja.java

--- src/productos/caja.java
+++ src/productos/caja.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class caja extends Producto{
 
-    public caja() {
+    public caja(String nombre, float precio, String marca, int stock, float PVP) {
+        super(nombre, precio, marca, stock, PVP);
     }
-
 }
+
