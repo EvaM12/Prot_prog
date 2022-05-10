b5cf26d2eee9e34c3ecf59ec12a76db6bfdc932c
4lvaro321 <99471055+4lvaro321@users.noreply.github.com>
2022-05-10 10:00:15 +0200

    Update Tienda.java

--- src/proyprog/Tienda.java
+++ src/proyprog/Tienda.java
@@ -6,49 +6,44 @@ import java.util.*;
  */
 public class Tienda {
 
-    /**
-     * Default constructor
-     */
     public Tienda() {
+        Pedidos = new ArrayList();
+        Productos = new ArrayList();
+        Empleados = new ArrayList();
+        Clientes = new ArrayList();
     }
 
+    private ArrayList<Pedido> Pedidos;
+    private ArrayList<Producto> Productos;
+
+    private ArrayList<Empleado> Empleados;
+    private ArrayList<Cliente> Clientes;
+
+    public void AñadirPedidos(Pedido p){
+        Pedidos.add(p);
+    }
+
+    public void AñadirProducto(Producto p) {
+        Productos.add(p);
+    }
     
-    private Pedido Pedidos[];
-
-    private Producto Productos[];
-
-    private Empleado Empleados[];
-
-    private Cliente Clientes[];
-
-    public void AñadirProductos() {
-        // TODO implement here
+    public void EliminarProducto(Producto p) {
+        Productos.remove(p);
     }
 
-    /**
-     *
-     */
-    public void EliminarProductos() {
-        // TODO implement here
+   
+    public void AñadirEmpleado(Empleado e) {
+        Empleados.add(e);
+    }
+    
+    public void EliminarEmpleado (Empleado e){
+        Empleados.remove(e);
     }
 
-    /**
-     *
-     */
-    public void AñadirEmpleados() {
-        // TODO implement here
+   
+    public void AñadirCliente(Cliente c) {
+        Clientes.add(c);
     }
-
-    /**
-     *
-     */
-    public void AñadirClientes() {
-        // TODO implement here
-    }
-
-    /**
-     *
-     */
     public void Contabilidad() {
         // TODO implement here
     }
