cc9c9e256e89d98e939f408d86598f1201fdb8fa
enriquecoblaz <99470933+enriquecoblaz@users.noreply.github.com>
2022-05-10 10:03:23 +0200

    Update Cliente.java

--- src/proyprog/Cliente.java
+++ src/proyprog/Cliente.java
@@ -1,20 +1,22 @@
-package proyprog;
-import java.util.*;
+package modelo.proyprog;
 
 /**
  * 
  */
-public class Cliente extends Persona{
+public class Cliente extends Persona {
 
     /**
      * Default constructor
      */
-    public Cliente() {
+    public Cliente(String dni, String nombre, String apellidos, long telefono, String email,String usuario, String contraseña, float saldo, boolean socio) {
+        super(dni, nombre, apellidos, telefono, email, usuario, contraseña);
+        this.saldo = saldo;
+        this.socio = socio;
     }
 
-    private float Saldo;
+    private float saldo;
 
-    private boolean Socio;
+    private boolean socio;
 
     public void hacerPedido() {
         // TODO implement here
