c78f24a407e62fa8da52aa8e214880797c163492
EvaM12 <61553044+EvaM12@users.noreply.github.com>
2022-05-12 13:37:24 +0200

    Add files via upload

---
+++ values.sql
@@ -0,0 +1,441 @@
+/*graficas*/
+insert into productos values (
+    'Gigabyte GeForce RTX 3070 Ti GAMING OC',/*nombre*/
+    889.89,/*precio*/
+    'NVIDIA',/*marca*/
+    10,/*stock*/
+    '8GB',/*memoria*/
+    '1830MHz',/*frequencia*/
+    'GDDR6X'/*tipo ram*/
+);
+
+insert into productos values (
+    'Gigabyte GeForce GTX 1660 SUPER OC',/*nombre*/
+    319.90,/*precio*/
+    'NVIDIA',/*marca*/
+    10,/*stock*/
+    '6GB',/*memoria*/
+    '1830MHz',/*frequencia*/
+    'GDDR6'/*tipo ram*/
+);
+
+insert into productos values (
+    'PULSE AMD Radeon RX',/*nombre*/
+    379.89,/*precio*/
+    'Sapphire',/*marca*/
+    10,/*stock*/
+    '8GB',/*memoria*/
+    '2044MHz',/*frequencia*/
+    'GDDR6'/*tipo ram*/
+);
+
+insert into productos values (
+    'ROG Strix GeForce RTX 3090 Gaming OC',/*nombre*/
+    1899.91,/*precio*/
+    'Asus /NVIDIA',/*marca*/
+    10,/*stock*/
+    '24GB',/*memoria*/
+    '2044MHz',/*frequencia*/
+    'GDDR6X'/*tipo ram*/
+);
+
+/*procesadores*/
+insert into productos values (
+    'Intel Core i5-10400F', /*NOMBRE*/
+	119.9, /*PRECIO*/
+	'Intel',	/*MARCA*/
+    10, /*Stock*/
+	'LGA 1200',	/*SOCKET*/
+	'2.9 GHz',	/*FRECUENCIA*/
+	6 ,	/*NUCLEOS*/
+	12 ,		/*HILOS*/
+);
+
+insert into productos values (
+    'Intel Core i5-12600K',
+	279.9,
+	'Intel',
+	10,
+	'LGA 1700',
+	'3.7GHz',
+	10,
+	16
+);
+
+insert into productos values (
+    'Intel Core i9-12900KS',	
+	789.89,
+	'Intel' ,
+	10,
+	'LGA 1700',
+	'5.5 GHz',
+	16,
+	24
+);
+
+insert into productos values (
+    'AMD Ryzen 5 5500',	
+	149.89,
+	'AMD',
+	10,
+	'AM4',
+	'3.6 GHz' ,
+	6,
+	12
+);
+
+/*disco duros*/
+insert into productos values(
+    'One Touch 2.5"',/*nombre*/
+    52.99,/*precio*/
+    'Seagate',/*marca*/
+    10,/*stock*/
+    'HDD',/*tipo*/
+    '1TB',/*capacidad*/
+    '?',/*vlec*/
+    '?'/*vesc*/
+);
+
+insert into productos values(
+    'Expansion Portable 2.5"',
+    95.99,
+    'Seagate',
+    10,
+    'HDD',
+    '4TB',
+    '?',
+    '?'
+);
+
+insert into productos values(
+    'Blue SN570',
+    89.99,
+    'WD',
+    10,
+    'SSD',
+    '500GB',
+    '3500 MB/s',
+    '2300 MB/s'
+);
+
+insert into productos values(
+    'NV1',
+    48.98,
+    'Kingston',
+    10,
+    'SSD',
+    '500GB',
+    '2100 MB/s',
+    '1700 MB/s'
+);
+
+insert into productos values(
+    'Black SN850',
+    137.99,
+    'WD',
+    10,
+    'SSD',
+    '1TB',
+    '7000MB/s',
+    '5300MB/s'
+);
+
+/*caja*/
+insert into productos values(
+    'iCUE 120T RGB',/*nombre*/
+    69.99,/*precio*/
+    'Corsair',/*marca*/
+    10/*stock*/
+);
+
+insert into productos values(
+    'Soul RGB',
+    31.99,
+    'Tempest',
+    10
+);
+
+insert into productos values(
+    'MAG Vampiric 010',
+    49.99,
+    'MSI',
+    10
+);
+
+insert into productos values(
+    'Krater',
+    69.95,
+    'Nfortec',
+    10
+);
+
+insert into productos values(
+    'MAG Vampiric 010M',
+    44.99,
+    'MSI',
+    10
+);
+
+/*alimentacion*/
+insert into productos values(
+    'MPG A750GF',
+    93.98,
+    'MSI',
+    10,
+    '80 Plus Gold',
+    750
+);
+
+insert into productos values(
+    'Urano VX',
+    50.25,
+    'Nox',
+    10,
+    '80 PLUS Bronze',
+    750
+);
+
+insert into productos values(
+    'HX1200',
+    219.99,
+    'Corsair',
+    10,
+    '80 Plus Platinum',
+    1200
+);
+
+insert into productos values(
+    'Hummer Alpha',
+    37.9,
+    'Nox',
+    10,
+    '80 Plus Bronce',
+    60
+);
+
+insert into productos values(
+    'ROG Strix 1000G',
+    199.99,
+    'Asus',
+    10,
+    '80 Plus Gold Modular',
+    1000
+);
+
+/*placa base*/
+insert into productos values(
+    'Gigabyte B660 GAMING X',/*nombre*/
+    139.9,/*precio*/
+    'Gigabyte',/*marca*/
+    10,/*stock*/
+    'LGA 1700',/*socket*/
+    'ATX'/*tipo*/
+);
+
+insert into productos values(
+    'Asus TUF GAMING B550',
+    149.89,
+    'Asus',
+    10,
+    'AM4',
+    'ATX'
+);
+
+insert into productos values(
+    'MSI MAG Z590 TOMAHAWK',
+    139.9,
+    'MSI',
+    10,
+    'LGA 1200',
+    'ATX'
+);
+/*ram*/
+
+insert into productos values(
+    'Kingston FURY Beast',/*nombre*/
+    39.53,/*precio*/
+    'Kingston',/*marca*/
+    10,/*stock*/
+    'DDR4',/*tipo*/
+    '3200 MHz',/*frequencia*/
+    '8 GB',/*capacidad*/
+    'CL16'/*latencia*/
+);
+insert into productos values(
+    'Corsair Vengeance LPX',
+    69.99,
+    'CORSAIR',
+    10,
+    'DDR4',
+    '3200 MHz' ,
+    '16 GB',
+    'CL16'
+);
+insert into productos values(
+    'Kingston FURY Beast',
+    57.57,
+    'Kingston',
+    10,
+    'DDR4',
+    '2666 MHz',
+    '8 GB 2x4 GB',
+    'CL16'
+);
+
+/*teclado*/
+insert into productos values(
+    'BlackWidow V3 Pro',/*nombre*/
+    183.77,/*precio*/
+    'Razer',/*marca*/
+    10,/*stock*/
+    'mecanico'/*tipo*/
+);
+
+insert into productos values(
+    'Doom H-Mech',
+    16.99,
+    'Tempest',
+    10,
+    'retroiluminado'
+);
+insert into productos values(
+    'BlackWidow V3 Teclado Gaming USA Green Switch Cuarzo Rosa',
+    102.23,
+    'Razer',
+    10,
+    'retroiluminado'
+);
+insert into productos values(
+    'Smart Backlit Teclado Bluetooth con Touchpad Plata',
+    28.23, 
+    'Subblim',
+    10,
+    'Touchpad'
+);
+insert into productos values(
+    'G84-5500 Teclado con TouchPad Negro',
+    154.75,
+    'Cherry',
+    10, 
+    'Touchpad'
+);
+
+/*refiregeracion*/
+insert into productos values(
+    'Liquid Cooler 360 RGB',/*nombre*/
+    58,/*precio*/
+    'Tempest',/*marca*/
+    10,/*stock*/
+    'liquida'/*tipo*/
+);
+insert into productos values(
+    'Hydrus V2 120 Red',
+    29.95,
+    'Nfortec',
+    10,
+    'liquida'
+);
+insert into productos values(
+    'Frost RGB Ventilador 120mm',
+    6.50,
+    'Aerocool',
+    10,
+    'aire'
+);
+insert into productos values(
+    'Force Ventilador 120mm',
+    5.51,
+    'Aerocool',
+    10,
+    'aire'
+);
+insert into productos values(
+    'MAG CoreLiquid 240R V2',
+    79.95,
+    'MSI',
+    10,
+    'liquida'
+);
+
+/*pantalla*/
+insert into productos values(
+    'Optix G27C7',/*nombre*/
+    199,/*precio*/
+    'MSI',/*marca*/
+    10,/*stock*/
+    '27"',/*tamaño*/
+    'FullHD'/*resolucion*/
+);
+insert into productos values(
+    'X27',
+    251.99,
+    'HP',
+    10,
+    '27"',
+    'FullHD'
+);
+insert into productos values(
+    'Icarus RGB',
+    319.98,
+    'Newskill',
+    10,
+    '27"',
+    'QHD'
+);
+insert into productos values(
+    'Optix G32CQ4',/*nombre*/
+    298.99,/*precio*/
+    'MSI',/*marca*/
+    10,/*stock*/
+    '31.5"',/*tamaño*/
+    'WQHD'/*resolucion*/
+);
+insert into productos values(
+    'Nitro KG2 KG272 S',
+    179,
+    'Acer',
+    10,
+    '27"',
+    'FullHD'
+);
+/*raton*/
+insert into productos values(
+    'Z3700 Blanco',/*nombre*/
+    11.98,/*precio*/
+    'HP',/*marcca*/
+    10,/*stock*/
+    'optico',/*sensor*/
+    1200/*dpi*/
+);
+insert into productos values(
+    'MS100 Paladin Ratón Gaming',
+    2.49,
+    'Tempest',
+    10,
+    'optico',
+    1600
+);
+insert into productos values(
+    'Omen Vector Essential Ratón Gaming',
+    19.99,
+    'HP',
+    10,
+    'omen',
+    7200
+);
+insert into productos values(
+    'X20 Vigilant RGB Ratón Gaming',
+    14.99,
+    'Tempest',
+    10,
+    '3389 IC',
+    16000
+);   
+insert into productos values(
+    'Basilisk V3 Ratón Gaming Óptico RGB',
+    51.10,
+    'Razer',
+    10,
+    'optico',
+    26000
+);
