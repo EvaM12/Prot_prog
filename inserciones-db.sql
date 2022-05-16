1ab2d2f392bc0931380d14c007f2a909098ff9bc
funerari <99471650+funerari@users.noreply.github.com>
2022-05-16 09:08:09 +0200

    Update inserciones-db.sql

--- inserciones-db.sql
+++ inserciones-db.sql
@@ -1,159 +1,658 @@
-create table persona (
-    dni_persona varchar(9),
-    nombre_persona varchar(50),
-    apellidos_persona varchar(50),
-    telefono_persona int,
-    email_persona varchar(50),
-    constraint persona_pk primary key (dni_persona)
+/*graficas*/
+insert into producto values (
+    'Gigabyte GeForce RTX 3070 Ti GAMING OC',/*nombre*/
+    889.89,/*precio*/
+    'NVIDIA',/*marca*/
+    10/*stock*/
 );
 
-create table cliente (
-    dni_cliente varchar(9),
-    socio_cliente boolean,
-    constraint cliente_pk primary key (dni_cliente),
-    constraint cliente_fk foreign key (dni_cliente) references persona(dni_persona)
-);
-    
-create table empleado (
-    dni_empleado varchar(9),  
-    salario_empleado float,
-    administrador_empleado boolean,
-    contraseña_empleado varchar(20),
-    constraint empleado_pk primary key (dni_empleado),
-    constraint empleado_fk foreign key (dni_empleado) references persona(dni_persona)
+insert into grafica values (
+    'Gigabyte GeForce RTX 3070 Ti GAMING OC',/*nombre*/
+    8,/*memoria*/
+    1830,/*frequencia*/
+    'GDDR6X'/*tipo ram*/
 );
 
-create table pedido (
-    ID_pedido int,
-    precio_total_pedido float,
-    fecha_pedido date,
-    constraint pedido_pk primary key (ID_pedido)
-);
-     
-create table venta (
-    ID_venta int,    
-    dni_empleado varchar(9),
-    dni_cliente varchar(9),
-    constraint venta_pk primary key (ID_venta),
-    constraint venta_fk1 foreign key (ID_venta) references pedido(ID_pedido),
-    constraint venta_fk2 foreign key (dni_empleado) references empleado(dni_empleado),
-    constraint venta_fk3 foreign key (dni_cliente) references cliente(dni_cliente)
-);
-    
-create table compra (
-    ID_compra int,    
-    proveedor_compra varchar(40),
-    constraint compra_pk primary key (ID_compra),
-    constraint compra_fk foreign key (ID_compra) references pedido(ID_pedido)
+insert into producto values (
+    'Gigabyte GeForce GTX 1660 SUPER OC',/*nombre*/
+    319.90,/*precio*/
+    'NVIDIA',/*marca*/
+    10/*stock*/
 );
 
-create table producto (
-    nombre_producto varchar(40),
-    precio_producto float,
-    marca_producto varchar(40),
-    stock_producto int,
-    pvp_producto float,
-    constraint producto_pk primary key (nombre_producto)
+insert into grafica values (
+    'Gigabyte GeForce GTX 1660 SUPER OC',/*nombre*/
+    8,/*memoria*/
+    1830,/*frequencia*/
+    'GDDR6X'/*tipo ram*/
 );
 
-create table placa_base (
-    nombre_placa_base varchar(40),
-    socket varchar(40),
-    tipo varchar(40),
-    constraint placa_base_pk primary key (nombre_placa_base),
-    constraint placa_base_fk foreign key (nombre_placa_base) references producto(nombre_producto)
+insert into producto values (
+    'PULSE AMD Radeon RX',/*nombre*/
+    379.89,/*precio*/
+    'Sapphire',/*marca*/
+    10/*stock*/
 );
 
-create table disco_duro (
-    nombre_disco_duro varchar(40),     
-    tipo_disco_duro varchar(50),
-    capacidad_disco_duro int,
-    velLectura_disco_duro int,
-    velEscritura_disco_duro int,
-    constraint disco_duro_pk primary key (nombre_disco_duro),
-    constraint disco_duro_fk foreign key (nombre_disco_duro) references producto(nombre_producto)
+insert into grafica values (
+    'PULSE AMD Radeon RX',/*nombre*/
+    8G,/*memoria*/
+    2044,/*frequencia*/
+    'GDDR6X'/*tipo ram*/
 );
 
-create table caja (
-    nombre varchar(40),
-    constraint caja_pk primary key (nombre) 
+insert into producto values (
+    'ROG Strix GeForce RTX 3090 Gaming OC',/*nombre*/
+    1899.91,/*precio*/
+    'Asus /NVIDIA',/*marca*/
+    10/*stock*/
 );
 
-create table grafica (
-    nombre_grafica varchar(40),     
-    memoria_grafica int,
-    frecuencia_grafica float(10),
-    tiporam_grafica varchar(7),
-    constraint grafica_pk primary key (nombre_grafica),
-    constraint grafica_fk foreign key (nombre_grafica) references producto(nombre_producto)
+insert into grafica values (
+    'ROG Strix GeForce RTX 3090 Gaming OC',/*nombre*/
+    24,/*memoria*/
+    2044,/*frequencia*/
+    'GDDR6X'/*tipo ram*/
 );
 
-create table pantalla (
-    nombre_pantalla varchar(40),   
-    tamaño_pantalla float,
-    resolucion_pantalla varchar(10),
-    constraint pantalla_pk primary key (nombre_pantalla),
-    constraint pantalla_fk foreign key (nombre_pantalla) references producto(nombre_producto)
+/*procesadores*/
+insert into producto values (
+    'Intel Core i5-10400F', /*NOMBRE*/
+	119.9, /*PRECIO*/
+	'Intel',	/*MARCA*/
+    10 /*Stock*/
 );
 
-CREATE TABLE procesador (
-    nombre_procesador varchar(40),    
-    socket_procesador varchar(10),
-    frecuencia_procesador float,
-    nucleos_procesador int,
-    hilos_procesador int,
-    constraint procesador_pk primary key (nombre_procesador),
-    constraint procesador_fk foreign key (nombre_procesador) references producto(nombre_producto)
-);
-    
-CREATE TABLE ram (
-    nombre_ram varchar(40),    
-    tipo_ram varchar(7),
-    frecuencia_ram float,
-    capacidad_ram int,
-    latencia_ram varchar(10),
-    constraint ram_pk primary key (nombre_ram),
-    constraint ram_fk foreign key (nombre_ram) references producto(nombre_producto)
-);
-    
-create table refrigeracion (
-    nombre_refrigeracion varchar(40),    
-    tipo_refrigeracion varchar(50),
-    constraint refrigeracion_pk primary key (nombre_refrigeracion),
-    constraint refrigeracion_fk foreign key (nombre_refrigeracion) references producto(nombre_producto)
+insert into procesador values (
+    'Intel Core i5-10400F', /*NOMBRE*/
+    'LGA 1200',	/*SOCKET*/
+	2.9 ,	/*FRECUENCIA*/
+	6 ,	/*NUCLEOS*/
+	12 ,		/*HILOS*/
 );
 
-create table fuente_alimentacion (
-    nombre_fuente_alimentacion varchar(40),    
-    certificacion_fuente_alimentacion varchar(50),
-    potencia_fuente_alimentacion varchar(10),
-    constraint fuente_alimentacion_pk primary key (nombre_fuente_alimentacion),
-    constraint fuente_alimentacion_fk foreign key (nombre_fuente_alimentacion) references producto(nombre_producto)
-);
-    
-create table raton (
-    nombre_raton varchar(40),   
-    sensor_raton varchar(50),
-    dpi_raton int,
-    constraint raton_pk primary key (nombre_raton),
-    constraint raton_fk foreign key (nombre_raton) references producto(nombre_producto)
+insert into producto values (
+    'Intel Core i5-12600K',
+	279.9,
+	'Intel',
+	10	
 );
 
-create table teclado (
-    nombre_teclado varchar(40),    
-    tipo_teclado varchar(50),
-    constraint teclado_pk primary key (nombre_teclado),
-    constraint teclado_fk foreign key (nombre_teclado) references producto(nombre_producto)
+insert into procesador values (
+    'Intel Core i5-12600K',
+    'LGA 1700',
+	3.7,
+	10,
+	16
 );
 
-create table DetallePedido (
-    ID_pedido int,
-    nombre_producto varchar(40),
-    Cantidad int,
-    constraint DetallePedido_pk primary key (ID_pedido, Linea, nombre_producto),
-    CONSTRAINT DetallePedido_FK1 FOREIGN KEY (ID_pedido) REFERENCES pedido(ID_pedido),
-    CONSTRAINT DetallePedido_FK2 FOREIGN KEY (nombre_producto) REFERENCES producto(nombre_producto)
+insert into producto values (
+    'Intel Core i9-12900KS',	
+	789.89,
+	'Intel' ,
+	10
+);
+
+insert into procesador values (
+    'Intel Core i9-12900KS',	
+    'LGA 1700',
+	5.5 ,
+	16,
+	24
+);
+
+insert into producto values (
+    'AMD Ryzen 5 5500',	
+	149.89,
+	'AMD',
+	10
+);
+
+insert into procesador values (
+    'AMD Ryzen 5 5500',	
+    'AM4',
+	3.6  ,
+	6,
+	12
+);
+
+/*disco duros*/
+insert into producto values(
+    'One Touch 2.5"',/*nombre*/
+    52.99,/*precio*/
+    'Seagate',/*marca*/
+    10/*stock*/
+);
+
+insert into disco_duro values (
+    'One Touch 2.5"',/*nombre*/
+    'HDD',/*tipo*/
+    1,/*capacidad*/
+    '?',/*vlec*/
+    '?'/*vesc*/
+);
+
+insert into producto values(
+    'Expansion Portable 2.5"',
+    95.99,
+    'Seagate',
+    10
+);
+
+insert into disco_duro values (
+    'Expansion Portable 2.5"',
+    'HDD',
+    4,
+    '?',
+    '?'
+);
+
+insert into producto values(
+    'Blue SN570',
+    89.99,
+    'WD',
+    10
+);
+
+insert into disco_duro values (
+    'Blue SN570',
+    'SSD',
+    500,
+    3500 ,
+    2300 
+);
+
+insert into producto values(
+    'NV1',
+    48.98,
+    'Kingston',
+    10
+);
+
+insert into disco_duro values (
+    'NV1',
+    'SSD',
+    500,
+    2100 ,
+    1700 
+);
+
+insert into producto values(
+    'Black SN850',
+    137.99,
+    'WD',
+    10
+);
+
+insert into disco_duro values (
+    'Black SN850',
+    'SSD',
+    1,
+    7000,
+    5300
+);
+
+/*caja*/
+insert into producto values(
+    'iCUE 120T RGB',/*nombre*/
+    69.99,/*precio*/
+    'Corsair',/*marca*/
+    10/*stock*/
+);
+
+insert into caja values (
+    'iCUE 120T RGB'/*nombre*/
+);
+
+insert into producto values(
+    'Soul RGB',
+    31.99,
+    'Tempest',
+    10
+);
+
+insert into caja values (
+    'Soul RGB'
+);
+
+insert into producto values(
+    'MAG Vampiric 010',
+    49.99,
+    'MSI',
+    10
+);
+
+insert into caja values (
+    'MAG Vampiric 010',
+);
+
+insert into producto values(
+    'Krater',
+    69.95,
+    'Nfortec',
+    10
+);
+
+insert into caja values (
+    'Krater'
+);
+
+insert into producto values(
+    'MAG Vampiric 010M',
+    44.99,
+    'MSI',
+    10
+);
+
+insert into caja values (
+    'MAG Vampiric 010M',
+);
+
+/*alimentacion*/
+insert into producto values(
+    'MPG A750GF',
+    93.98,
+    'MSI',
+    10
+);
+
+insert into alimentacion values (
+    'MPG A750GF',
+    '80 Plus Gold',
+    750
+);
+
+insert into producto values(
+    'Urano VX',
+    50.25,
+    'Nox',
+    10
+);
+
+insert into alimentacion values (
+    'Urano VX',
+    '80 PLUS Bronze',
+    750
+);
+
+insert into producto values(
+    'HX1200',
+    219.99,
+    'Corsair',
+    10
+);
+
+insert into alimentacion values (
+    'HX1200',
+    '80 Plus Platinum',
+    1200
+);
+
+insert into producto values(
+    'Hummer Alpha',
+    37.9,
+    'Nox',
+    10
+);
+
+insert into alimentacion values (
+    'Hummer Alpha',
+    '80 Plus Bronce',
+    60
+);
+
+insert into producto values(
+    'ROG Strix 1000G',
+    199.99,
+    'Asus',
+    10
+);
+
+insert into alimentacion values (
+    'ROG Strix 1000G',
+    '80 Plus Gold Modular',
+    1000
+);
+
+/*placa base*/
+insert into producto values(
+    'Gigabyte B660 GAMING X',/*nombre*/
+    139.9,/*precio*/
+    'Gigabyte',/*marca*/
+    10/*stock*/ 
+);
+
+insert into placa_base values (
+    'Gigabyte B660 GAMING X',
+    'LGA 1700',/*socket*/
+    'ATX'/*tipo*/
+);
+
+insert into producto values(
+    'Asus TUF GAMING B550',
+    149.89,
+    'Asus',
+    10
+);
+
+insert into placa_base values (
+    'Asus TUF GAMING B550',
+    'AM4',
+    'ATX'
+);
+
+insert into producto values(
+    'MSI MAG Z590 TOMAHAWK',
+    139.9,
+    'MSI',
+    10
+);
+
+insert into placa_base values (
+    'MSI MAG Z590 TOMAHAWK',
+    'LGA 1200',
+    'ATX'
+);
+
+/*ram*/
+
+insert into producto values(
+    'Kingston FURY Beast',/*nombre*/
+    39.53,/*precio*/
+    'Kingston',/*marca*/
+    10/*stock*/
+
+);
+
+insert into ram values (
+    'Kingston FURY Beast',
+    'DDR4',/*tipo*/
+    3200 ,/*frequencia*/
+    8 ,/*capacidad*/
+    'CL16'/*latencia*/
+);
+
+insert into producto values(
+    'Corsair Vengeance LPX',
+    69.99,
+    'CORSAIR',
+    10
+);
+
+insert into ram values (
+    'Corsair Vengeance LPX',
+    'DDR4',
+    3200  ,
+    16 ,
+    'CL16'
+);
+
+insert into producto values(
+    'Kingston FURY Beast',
+    57.57,
+    'Kingston',
+    10
+);
+
+insert into ram values (
+    'Kingston FURY Beast',
+    'DDR4',
+    2666 ,
+    8   ,
+    'CL16'
+);
+
+/*teclado*/
+insert into producto values(
+    'BlackWidow V3 Pro',/*nombre*/
+    183.77,/*precio*/
+    'Razer',/*marca*/
+    10/*stock*/
+);
+
+inssert into teclado values (
+    'BlackWidow V3 Pro',
+    'mecanico'/*tipo*/
+);
+
+insert into producto values(
+    'Doom H-Mech',
+    16.99,
+    'Tempest',
+    10
+);
+
+inssert into teclado values (
+    'Doom H-Mech',
+    'retroiluminado'
+);
+
+insert into producto values(
+    'BlackWidow V3 Teclado Gaming USA Green Switch Cuarzo Rosa',
+    102.23,
+    'Razer',
+    10
+);
+
+inssert into teclado values (
+    'BlackWidow V3 Teclado Gaming USA Green Switch Cuarzo Rosa',
+    'retroiluminado'
+);
+
+insert into producto values(
+    'Smart Backlit Teclado Bluetooth con Touchpad Plata',
+    28.23, 
+    'Subblim',
+    10
+);
+
+inssert into teclado values (
+    'Smart Backlit Teclado Bluetooth con Touchpad Plata',
+    'Touchpad'
+);
+
+insert into producto values(
+    'G84-5500 Teclado con TouchPad Negro',
+    154.75,
+    'Cherry',
+    10
+);
+
+inssert into teclado values (
+    'G84-5500 Teclado con TouchPad Negro',
+    'Touchpad'
+);
+
+/*refiregeracion*/
+insert into producto values(
+    'Liquid Cooler 360 RGB',/*nombre*/
+    58,/*precio*/
+    'Tempest',/*marca*/
+    10/*stock*/
+);
+
+insert into refiregeracion values (
+    'Liquid Cooler 360 RGB',/*nombre*/
+    'liquida'/*tipo*/
+);
+
+insert into producto values(
+    'Hydrus V2 120 Red',
+    29.95,
+    'Nfortec',
+    10
+);
+
+insert into refiregeracion values (
+    'Hydrus V2 120 Red',
+    'liquida'
+);
+
+insert into producto values(
+    'Frost RGB Ventilador 120mm',
+    6.50,
+    'Aerocool',
+    10
+);
+
+insert into refiregeracion values (
+    'Frost RGB Ventilador 120mm',
+    'aire'
+);
+
+insert into producto values(
+    'Force Ventilador 120mm',
+    5.51,
+    'Aerocool',
+    10
+);
+
+insert into refiregeracion values (
+    'Force Ventilador 120mm',
+    'aire'
+);
+
+insert into producto values(
+    'MAG CoreLiquid 240R V2',
+    79.95,
+    'MSI',
+    10
+);
+
+insert into refiregeracion values (
+    'MAG CoreLiquid 240R V2',
+    'liquida'
 );
 
 
+/*pantalla*/
+insert into producto values(
+    'Optix G27C7',/*nombre*/
+    199,/*precio*/
+    'MSI',/*marca*/
+    10/*stock*/
+);
 
+insert into monitor values (
+    'Optix G27C7',/*nombre*/
+    27,/*tamaño*/
+    'FullHD'/*resolucion*/
+);
+
+insert into producto values(
+    'X27',
+    251.99,
+    'HP',
+    10
+);
+
+insert into monitor values (
+    'X27',
+    27,
+    'FullHD'
+);
+
+insert into producto values(
+    'Icarus RGB',
+    319.98,
+    'Newskill',
+    10
+);
+
+insert into monitor values (
+    'Icarus RGB',
+    27,
+    'QHD'
+);
+
+insert into producto values(
+    'Optix G32CQ4',/*nombre*/
+    298.99,/*precio*/
+    'MSI',/*marca*/
+    10/*stock*/
+);
+
+insert into monitor values (
+    'Optix G32CQ4',/*nombre*/
+    31.5,/*tamaño*/
+    'WQHD'/*resolucion*/
+);
+
+insert into producto values(
+    'Nitro KG2 KG272 S',
+    179,
+    'Acer',
+    10
+);
+
+insert into monitor values (
+    'Nitro KG2 KG272 S',
+    27,
+    'FullHD'
+);
+
+/*raton*/
+insert into producto values(
+    'Z3700 Blanco',/*nombre*/
+    11.98,/*precio*/
+    'HP',/*marcca*/
+    10/*stock*/
+);
+
+insert into raton values (
+    'Z3700 Blanco',/*nombre*/
+    'optico',/*sensor*/
+    1200/*dpi*/
+);
+
+insert into producto values(
+    'MS100 Paladin Ratón Gaming',
+    2.49,
+    'Tempest',
+    10
+);
+
+insert into raton values (
+    'MS100 Paladin Ratón Gaming',
+    'optico',
+    1600
+);
+
+insert into producto values(
+    'Omen Vector Essential Ratón Gaming',
+    19.99,
+    'HP',
+    10
+);
+
+insert into raton values (
+    'Omen Vector Essential Ratón Gaming',
+    'omen',
+    7200
+);
+
+insert into producto values(
+    'X20 Vigilant RGB Ratón Gaming',
+    14.99,
+    'Tempest',
+    10
+); 
+
+insert into raton values (
+    'X20 Vigilant RGB Ratón Gaming',
+    '3389 IC',
+    16000 
+);
+
+insert into producto values(
+    'Basilisk V3 Ratón Gaming Óptico RGB',
+    51.10,
+    'Razer',
+    10
+);
+
+insert into raton values (
+    'Basilisk V3 Ratón Gaming Óptico RGB',
+    'optico',
+    26000
+);
